ff3b35b9ad1548099a1e5b2cb348809d23d8c84e
SigmaChad <qinghangcom@gmail.com>
2024-04-21 16:15:43 +0800

    Update MainCode.ino

--- 2024_IDE_PRIME/Main/MainCode/MainCode.ino
+++ 2024_IDE_PRIME/Main/MainCode/MainCode.ino
@@ -1,12 +1,15 @@
 
 #include <LiquidCrystal_I2C.h>
+#include <Servo.h> 
 
 LiquidCrystal_I2C lcd(0x3F, 16, 2); 
 
 const int AirValue = 620;  
 const int WaterValue = 310; 
 int soilMoistureValue = 0;
-int soilmoisturepercent=0;
+int soilmoisturepercent = 0;
+Servo servo;
+int angle_barrier = 90;
 
 byte customChar[8] = {
 	0b00100,
@@ -38,11 +41,61 @@ void setup()
   lcd.backlight();
   lcd.createChar(0, customChar);
   lcd.createChar(1, customChar1);
-
+  servo.attach(A9);
+  servo.write(0);
 }
 
 void loop()
 {
+  LCD_print();
+  SoilMoisture_Capacitive();
+
+}
+
+void SoilMoisture_Capacitive() {
+  soilMoistureValue = analogRead(A0);
+  Serial.print("Soil Moisture Value: ");
+  Serial.println(soilMoistureValue);
+  soilmoisturepercent = map(soilMoistureValue, AirValue, WaterValue, 0, 100);
+  lcd.setCursor(7, 1);
+  if(soilmoisturepercent >= 80)
+  {
+    Serial.print("Moisture Percentage: ");
+    Serial.println("100 %");
+    lcd.print("FULL");
+    angle_barrier = 90;
+    Servo_Code();
+
+  }
+  else if(soilmoisturepercent <=0)
+  {
+    Serial.print("Moisture Percentage: ");
+    Serial.println("0 %");
+    lcd.print("NULL");
+    angle_barrier = 90;
+  }
+  else if(soilmoisturepercent >0 && soilmoisturepercent < 100)
+  {
+    Serial.print("Moisture Percentage: ");
+    Serial.print(soilmoisturepercent);
+    Serial.println("%");
+    lcd.print(soilmoisturepercent);
+    angle_barrier = 0;
+    Servo_Code();
+  }
+  lcd.print("%");
+  delay(1000); 
+}
+
+void Servo_Code() {
+
+  for (int pos = 0; pos != angle_barrier; pos += 1) {
+    servo.write(pos);
+    delay(20);
+  }
+}
+
+void LCD_print(){
   lcd.clear();
   lcd.setCursor(0, 0);         
   lcd.print("Landslide Ahead");
@@ -55,39 +108,4 @@ void loop()
   lcd.setCursor(14, 1);
   lcd.write((byte)1);
   lcd.setCursor(7, 1);
-  SoilMoisture_Capacitive();
-
-  
-
-}
-
-void SoilMoisture_Capacitive() {
-  soilMoistureValue = analogRead(A0);
-  Serial.print("Soil Moisture Value: ");
-  Serial.println(soilMoistureValue);
-  soilmoisturepercent = map(soilMoistureValue, AirValue, WaterValue, 0, 100);
-  lcd.setCursor(7, 1);
-  if(soilmoisturepercent >= 100)
-  {
-    Serial.print("Moisture Percentage: ");
-    Serial.println("100 %");
-    lcd.print("FULL");
-
-  }
-  else if(soilmoisturepercent <=0)
-  {
-    Serial.print("Moisture Percentage: ");
-    Serial.println("0 %");
-    lcd.print("NULL");
-  }
-  else if(soilmoisturepercent >0 && soilmoisturepercent < 100)
-  {
-    Serial.print("Moisture Percentage: ");
-    Serial.print(soilmoisturepercent);
-    Serial.println("%");
-    lcd.print(soilmoisturepercent);
-  }
-  lcd.print("%");
-  delay(1000); 
-
 }
